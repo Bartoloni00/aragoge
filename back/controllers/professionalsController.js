b9f85a34d04172c1e8be747d01d42bd9bede9db9
Abraham Bartoloni <116020038+Bartoloni00@users.noreply.github.com>
2023-10-28 13:38:56 -0300

    create, delete and update professionals
    se finalizo la version basica del CRUD de profesionales

--- back/controllers/professionalsController.js
+++ back/controllers/professionalsController.js
@@ -1,27 +1,74 @@
 import { profesionalModel } from "../models/profesionalModel.js"
 
 export class profesionalController {
+    /**
+     * Obtiene todas las cervezas o las cervezas de un tipo específico.
+     * @param {object} req - Objeto de solicitud HTTP.
+     * @param {object} res - Objeto de respuesta HTTP.
+     * @returns {Promise<void>} - No devuelve un valor directamente, pero envía una respuesta JSON al cliente.
+     */
     static async listAll(req, res)
     {
         const profesional = await profesionalModel.getAll()
         if(profesional) return res.json(profesional)
         res.status(404).json({message: 'Profesional not found'})
     }
+
+    /**
+     * Obtiene todas las cervezas o las cervezas de un tipo específico.
+     * @param {object} req - Objeto de solicitud HTTP.
+     * @param {object} res - Objeto de respuesta HTTP.
+     * @returns {Promise<void>} - No devuelve un valor directamente, pero envía una respuesta JSON al cliente.
+     */
     static async getByID(req, res)
     {
         const id = req.params.id
         res.send(await profesionalModel.getByID({id: id}))
     }
+
+    /**
+     * Obtiene todas las cervezas o las cervezas de un tipo específico.
+     * @param {object} req - Objeto de solicitud HTTP.
+     * @param {object} res - Objeto de respuesta HTTP.
+     * @returns {Promise<void>} - No devuelve un valor directamente, pero envía una respuesta JSON al cliente.
+     */
     static async create(req, res)
     {
-
+        profesionalModel.create({profesional: req.body})
+        .then(createdProfessional => {
+            res.status(201).send(createdProfessional)
+        })
+        .catch(err => {
+            res.status(500).json({"message": "Error al intentar agregar al profesional", err})
+        })
     }
+
+    /**
+     * Obtiene todas las cervezas o las cervezas de un tipo específico.
+     * @param {object} req - Objeto de solicitud HTTP.
+     * @param {object} res - Objeto de respuesta HTTP.
+     * @returns {Promise<void>} - No devuelve un valor directamente, pero envía una respuesta JSON al cliente.
+     */
     static async delete(req, res)
     {
-
+        const id = req.params.id
+        profesionalModel.delete({id:id})
+            .then(json => {
+                res.status(200).json({"message": json})
+            })
+            .catch(err => {
+                res.status(500).json({"message": `Ocurrio un error al eliminar al profesional: ${err}`})
+            })
     }
     static async update(req, res)
     {
-
+        const id = req.params.id
+        profesionalModel.update({id: id, datos: req.body})
+            .then(updateProfesional => {
+                res.status(200).json({"message": `Profesional editado exitosamente: ${id}`, updateProfesional})
+            })
+            .catch(err => {
+                res.status(500).json({"message": `ocurrio un error al editar al profesional`, err})
+            })
     }
 }
--- back/models/profesionalModel.js
+++ back/models/profesionalModel.js
@@ -16,7 +16,56 @@ export class profesionalModel
         try {
             return db.collection('professionals').findOne({_id: new ObjectId(id)})
         } catch (error) {
-            return {"message": 'Ocurrio un error al intentar obtener el documento', error}
+            return {"message": 'Ocurrio un error al intentar obtener el documento','error': error}
+        }
+    }
+
+    static async create({profesional})
+    {
+        const newProfesional = {
+            "description": profesional.description,
+            "synopsis": profesional.synopsis,
+            "banner": profesional.banner ?? "https://picsum.photos/400/225",
+            "alt": profesional.alt ?? "Banner de entrenador",
+            "subscribers": profesional.subscribers ?? 0,
+            "user": "653ab688b767888b31dfea42", // esto tendra que ser una conexion de 1 a 1 por lo que no lo tocare hasta tener los usuarios
+            "especialiti": "entrenador" // esto tambien tiene una relacion con la coleccion specialities
+        } 
+        try {
+            const profesional = await db.collection('professionals').insertOne(newProfesional)
+            newProfesional._id = profesional.insertedId 
+            return newProfesional
+        } catch (error) {
+            return {"message": `No se ha podido agregar al profesional en la base de datos ${error}`}
+        }
+    }
+
+    static async delete ({id})
+    {
+        try {
+            await db.collection('professionals').deleteOne({ _id: new ObjectId(id) })
+        } catch (error) {
+            return {'message': `El profesional con el id: ${id} no pudo ser eliminado`, error}
+        }
+        return {'message': `El profesional con el id: ${id} fue eliminado exitosamente`}
+    }
+
+    static async update({id, datos})
+    {
+        try {
+            const updateProfesional = {
+                "description": datos.description,
+                "synopsis": datos.synopsis,
+                "banner": datos.banner ?? "https://picsum.photos/400/225",
+                "alt": datos.alt ?? "Banner de entrenador",
+                "subscribers": datos.subscribers ?? 0,
+                "especialiti": datos.especialidad // esto tambien tiene una relacion con la coleccion specialities
+            } 
+    
+            const result = await db.collection('professionals').updateOne({ _id: new ObjectId(id) }, { $set: updateProfesional });
+            return result;
+        } catch (error) {
+            return {'error': `ocurrio un error al editar al profesional: ${error}`}
         }
     }
 }
